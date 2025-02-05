74aad1fd8af08149852696c98b91edcf2a37d494
SkyMazter <comunidadoscar@gmail.com>
2025-02-04 23:37:47 -0500

    changed front end

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,89 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Offcanvas from "react-bootstrap/Offcanvas";
+
+import {
+  BsThreeDotsVertical,
+  BsMoonStarsFill,
+  BsMoonStars,
+} from "react-icons/bs";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [isDark, setIsDark] = useState(false);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
+  const [show, setShow] = useState(false);
+
+  function handleToggle() {
+    if (isDark == true) setIsDark(false);
+    else setIsDark(true);
   }
 
+  const handleShow = () => {
+    if (show == true) setShow(false);
+    else setShow(true);
+  };
+
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+    <Container
+      fluid
+      className="quicksand"
+      style={{
+        height: "100vh",
+        backgroundColor: isDark ? "black" : "white",
+        color: isDark ? "white" : "black",
+        transition: "0.3s",
+        overflow: "scroll",
+      }}
+    >
+      <Row>
+        <Col xs={2} className="d-flex justify-content-start align-items-center">
+          {" "}
+          {isDark ? (
+            <BsMoonStarsFill className="bttn" onClick={handleToggle} />
+          ) : (
+            <BsMoonStars className="bttn" onClick={handleToggle} />
+          )}
+        </Col>
+        <Col className="d-flex justify-content-center">
+          {" "}
+          <h1>Example App</h1>{" "}
+        </Col>
+        <Col xs={2} className="d-flex justify-content-end align-items-center">
+          <BsThreeDotsVertical className="bttn" onClick={handleShow} />
+        </Col>
+      </Row>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
+      <Offcanvas
+        className="quicksand"
+        show={show}
+        onHide={handleShow}
+        placement="end"
+        style={{
+          backgroundColor: isDark && "grey",
+          color: isDark && "white",
         }}
       >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>
+            <h3>Menu</h3>
+          </Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Container>
+            <Row className="fs-5 py-1 menu-bttn">Home</Row>
+
+            <Row className="fs-5 py-1 menu-bttn">About</Row>
+
+            <Row className="fs-5 py-1 menu-bttn">Settings</Row>
+          </Container>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </Container>
   );
 }
 
